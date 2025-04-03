501d3318dfe861d6cf3d7e9f5eaff8519a4231df
dennis <denya10.02@googlemail.com>
2025-04-03 19:42:15 +0200

    Homework DemoWebShop

---
+++ src/test/java/com/ait/qa55/DemoWebShopTest.java
@@ -0,0 +1,27 @@
+package com.ait.qa55;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DemoWebShopTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com/");
+    }
+    @Test
+    public void openDemoWebShop(){
+        System.out.println("Hello Webshop");
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown(){
+        driver.quit();
+    }
+}
